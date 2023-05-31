f88a13251800bcda83831f930d38a35faa80d970
shaevtchac <shaevtchac@gmail.com>
2023-05-31 10:39:01 +0200

    first

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      sm: '530px',
+      md: '700px',
+      lg: '900px',
+      xl: '1280px',
+    },
+
+    extend: {
+      colors: {
+        primary: '#33d2fd',
+        primarytr: '#33d2fd88',
+        secondary: '#233148',
+        menubg: '#23314888',
+      },
+      fontFamily: {
+        workSans: ['Work Sans', 'sans-serif'],
+      },
+    },
+  },
+
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+};
